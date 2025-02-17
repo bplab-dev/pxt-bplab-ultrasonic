6f313a11f0dcc3ca61f71b9dd963b00980d21de3
bplab-dev <dev@bplab.kr>
2025-02-17 11:20:07 +0900

    tooltip 추가

--- main.ts
+++ main.ts
@@ -20,6 +20,7 @@ namespace ultrasonic {
      * @param maxCmDistance maximum distance in centimeters (default is 500)
      */
     //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
+    //% tooltip="Measures the distance to an object using an ultrasonic sensor."
     export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
         // send pulse
         pins.setPull(trig, PinPullMode.PullNone);
