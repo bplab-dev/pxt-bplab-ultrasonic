34ead666c33a3ed0477f484a9702159c96926914
bplab-dev <dev@bplab.kr>
2025-02-26 10:52:43 +0900

    줄 바꿈 성공!

--- main.ts
+++ main.ts
@@ -22,40 +22,10 @@ namespace ultrasonic {
      * @param echo Echo pin
      * @param unit Desired conversion unit
      */
-    //% blockId=sonar_ping block="Ping trig %trig|echo %echo|Unit %unit"
+    //% blockId=sonar_ping block="Ping|trig %trig|echo %echo|Unit %unit"
     //% tooltip="Measures the distance to an object using an ultrasonic sensor."
+    //% inlineInputMode=external
     export function ping(trig: DigitalPin = DigitalPin.P14, echo: DigitalPin = DigitalPin.P15, unit: PingUnit): number {
-        const maxCmDistance = 500
-
-        // send pulse
-        pins.setPull(trig, PinPullMode.PullNone);
-        pins.digitalWritePin(trig, 0);
-        control.waitMicros(2);
-        pins.digitalWritePin(trig, 1);
-        control.waitMicros(10);
-        pins.digitalWritePin(trig, 0);
-
-        // read pulse
-        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
-
-        switch (unit) {
-            case PingUnit.Centimeters: return Math.idiv(d, 58);
-            case PingUnit.Inches: return Math.idiv(d, 148);
-            default: return d;
-        }
-    }
-
-    /**
-     * Send a ping and get the echo time (in microseconds) as a result
-     * @param trig Trigger pin
-     * @param echo Echo pin
-     * @param unit Desired conversion unit
-     */
-    //% subcategory="TEST"
-    //% color="#003eff"
-    //% blockId=sonar_ping2 block="Ping2 trig %trig|echo %echo|Unit %unit"
-    //% tooltip="Measures the distance to an object using an ultrasonic sensor."
-    export function ping2(trig: DigitalPin = DigitalPin.P14, echo: DigitalPin = DigitalPin.P15, unit: PingUnit): number {
         const maxCmDistance = 500
 
         // send pulse
